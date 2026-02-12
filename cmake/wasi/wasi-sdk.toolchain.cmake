9fcbd7dd4f3f51ad079e34d1d80e2e56db5a0ca7
Rio Martinez <rio@riomartinez.com>
2026-02-12 19:56:19 +0100

    chore: bump wasi-sdk to v30 and enable sjlj

--- cmake/wasi/wasi-sdk.toolchain.cmake
+++ cmake/wasi/wasi-sdk.toolchain.cmake
@@ -4,17 +4,18 @@ FetchContent_Declare(
   wasi_sdk_toolchain
   SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/wasi-sdk"
   GIT_REPOSITORY https://github.com/rioam2/wasi-sdk-toolchain.git
-  GIT_TAG 1b25d4dfd68f963c855e293ec94d6a79dcb5c5c1
+  GIT_TAG b032d4fe3c919e0b24f45ccf482be09bf2a9950a
 )
 FetchContent_MakeAvailable(wasi_sdk_toolchain)
 
 include("${wasi_sdk_toolchain_SOURCE_DIR}/wasi-sdk.toolchain.cmake")
 
 initialize_wasi_toolchain(
-  WIT_BINDGEN_TAG "v0.43.0"
-  WASMTIME_TAG "v35.0.0"
-  WASM_TOOLS_TAG "v1.236.0"
-  WASI_SDK_TAG "wasi-sdk-27"
+  WIT_BINDGEN_TAG "v0.53.1"
+  WASMTIME_TAG "v41.0.3"
+  WASM_TOOLS_TAG "v1.245.1"
+  WASI_SDK_TAG "wasi-sdk-30"
   TARGET_TRIPLET "wasm32-wasi"
+  ENABLE_EXPERIMENTAL_SETJMP ON
   ENABLE_EXPERIMENTAL_STUBS ON
 )
