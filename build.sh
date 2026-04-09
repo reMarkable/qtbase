rioam2/qtbase-wasm32-wasi

chore: add build scripts and github action
Adds dockerfile for building in a debian image and
adds a build script for ease of use compiling for
the first time.

A root container is used to install wasi-sdk into
the clang-runtimes directory.

A github workflow is added to smoke test all
pushes and pull requests. Resulting static
library is uploaded to the run as an artifact.

build.sh
@@ -1,0 +1,345 @@
+#!/usr/bin/env bash
+
+set -e
+set -o xtrace
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cmake . \
+    -B ./build/host \
+    -G Ninja \
+    `# Build configuration` \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF \
+    -DQT_BUILD_BENCHMARKS=OFF \
+    -DQT_FEATURE_accessibility=OFF \
+    -DQT_FEATURE_androiddeployqt=OFF \
+    -DQT_FEATURE_animation=OFF \
+    -DQT_FEATURE_backtrace=OFF \
+    -DQT_FEATURE_dbus=OFF \
+    -DQT_FEATURE_debug=OFF \
+    -DQT_FEATURE_developer_build=OFF \
+    -DQT_FEATURE_dom=OFF \
+    -DQT_FEATURE_harfbuzz=OFF \
+    -DQT_FEATURE_ico=OFF \
+    -DQT_FEATURE_icu=OFF \
+    -DQT_FEATURE_network=OFF \
+    -DQT_FEATURE_pdf=OFF \
+    -DQT_FEATURE_timezone=OFF \
+    -DQT_FEATURE_translation=OFF \
+    -DQT_FEATURE_widgets=OFF
+
+
+cmake --build ./build/host --parallel
+cmake --install ./build/host --prefix "./build/host-install"
+
+cmake . \
+    -B ./build/wasm32-wasi \
+    -G Ninja \
+    `# Build configuration` \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_FIND_PACKAGE_TARGETS_GLOBAL=ON \
+    -DQT_HOST_PATH="$script_dir/build/host" \
+    -DQT_HOST_PATH_CMAKE_DIR:PATH="$script_dir/build/host" \
+    -DCMAKE_TOOLCHAIN_FILE="$script_dir/cmake/wasi/wasi-sdk.toolchain.cmake" \
+    -DQT_QMAKE_TARGET_MKSPEC=wasm32-wasi-clang-libc++-32 \
+    -DQT_USE_DEFAULT_CMAKE_OPTIMIZATION_FLAGS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF \
+    -DQT_BUILD_BENCHMARKS=OFF \
+    -DINSTALL_BINDIR:STRING=bin \
+    -DINSTALL_LIBEXECDIR:STRING=bin \
+    `# Feature configuration` \
+    -DQT_FEATURE_accessibility_atspi_bridge=OFF \
+    -DQT_FEATURE_accessibility=OFF \
+    -DQT_FEATURE_action=OFF \
+    -DQT_FEATURE_aesni=OFF \
+    -DQT_FEATURE_android_style_assets=OFF \
+    -DQT_FEATURE_androiddeployqt=OFF \
+    -DQT_FEATURE_animation=OFF \
+    -DQT_FEATURE_appstore_compliant=OFF \
+    -DQT_FEATURE_arm_crc32=OFF \
+    -DQT_FEATURE_arm_crypto=OFF \
+    -DQT_FEATURE_avx=OFF \
+    -DQT_FEATURE_avx2=OFF \
+    -DQT_FEATURE_avx512bw=OFF \
+    -DQT_FEATURE_avx512cd=OFF \
+    -DQT_FEATURE_avx512dq=OFF \
+    -DQT_FEATURE_avx512er=OFF \
+    -DQT_FEATURE_avx512f=OFF \
+    -DQT_FEATURE_avx512ifma=OFF \
+    -DQT_FEATURE_avx512pf=OFF \
+    -DQT_FEATURE_avx512vbmi=OFF \
+    -DQT_FEATURE_avx512vbmi2=OFF \
+    -DQT_FEATURE_avx512vl=OFF \
+    -DQT_FEATURE_backtrace=OFF \
+    -DQT_FEATURE_cborstreamreader=ON \
+    -DQT_FEATURE_cborstreamwriter=ON \
+    -DQT_FEATURE_clipboard=OFF \
+    -DQT_FEATURE_clock_gettime=OFF \
+    -DQT_FEATURE_clock_monotonic=OFF \
+    -DQT_FEATURE_close_range=OFF \
+    -DQT_FEATURE_colornames=OFF \
+    -DQT_FEATURE_commandlineparser=ON \
+    -DQT_FEATURE_concatenatetablesproxymodel=ON \
+    -DQT_FEATURE_concurrent=OFF \
+    -DQT_FEATURE_cpp_winrt=OFF \
+    -DQT_FEATURE_cross_compile=ON \
+    -DQT_FEATURE_cssparser=ON \
+    -DQT_FEATURE_ctf=OFF \
+    -DQT_FEATURE_cursor=ON \
+    -DQT_FEATURE_cxx11_future=OFF \
+    -DQT_FEATURE_cxx17_filesystem=OFF \
+    -DQT_FEATURE_cxx20=OFF \
+    -DQT_FEATURE_cxx2a=OFF \
+    -DQT_FEATURE_cxx2b=OFF \
+    -DQT_FEATURE_datestring=ON \
+    -DQT_FEATURE_datetimeparser=ON \
+    -DQT_FEATURE_dbus_linked=OFF \
+    -DQT_FEATURE_dbus=OFF \
+    -DQT_FEATURE_debug_and_release=OFF \
+    -DQT_FEATURE_debug=OFF \
+    -DQT_FEATURE_desktopservices=ON \
+    -DQT_FEATURE_developer_build=OFF \
+    -DQT_FEATURE_direct2d=OFF \
+    -DQT_FEATURE_direct2d1_1=OFF \
+    -DQT_FEATURE_directfb=OFF \
+    -DQT_FEATURE_directwrite=OFF \
+    -DQT_FEATURE_directwrite3=OFF \
+    -DQT_FEATURE_dladdr=OFF \
+    -DQT_FEATURE_dlopen=OFF \
+    -DQT_FEATURE_dom=ON \
+    -DQT_FEATURE_doubleconversion=ON \
+    -DQT_FEATURE_draganddrop=OFF \
+    -DQT_FEATURE_drm_atomic=OFF \
+    -DQT_FEATURE_dynamicgl=OFF \
+    -DQT_FEATURE_easingcurve=ON \
+    -DQT_FEATURE_egl_x11=OFF \
+    -DQT_FEATURE_egl=OFF \
+    -DQT_FEATURE_eglfs_brcm=OFF \
+    -DQT_FEATURE_eglfs_egldevice=OFF \
+    -DQT_FEATURE_eglfs_gbm=OFF \
+    -DQT_FEATURE_eglfs_mali=OFF \
+    -DQT_FEATURE_eglfs_openwfd=OFF \
+    -DQT_FEATURE_eglfs_rcar=OFF \
+    -DQT_FEATURE_eglfs_viv_wl=OFF \
+    -DQT_FEATURE_eglfs_viv=OFF \
+    -DQT_FEATURE_eglfs_vsp2=OFF \
+    -DQT_FEATURE_eglfs_x11=OFF \
+    -DQT_FEATURE_eglfs=OFF \
+    -DQT_FEATURE_etw=OFF \
+    -DQT_FEATURE_evdev=OFF \
+    -DQT_FEATURE_eventfd=OFF \
+    -DQT_FEATURE_f16c=OFF \
+    -DQT_FEATURE_filesystemiterator=OFF \
+    -DQT_FEATURE_filesystemmodel=OFF \
+    -DQT_FEATURE_filesystemwatcher=OFF \
+    -DQT_FEATURE_fontconfig=OFF \
+    -DQT_FEATURE_force_asserts=OFF \
+    -DQT_FEATURE_force_debug_info=OFF \
+    -DQT_FEATURE_forkfd_pidfd=OFF \
+    -DQT_FEATURE_framework=OFF \
+    -DQT_FEATURE_freetype=ON \
+    -DQT_FEATURE_futimens=ON \
+    -DQT_FEATURE_future=OFF \
+    -DQT_FEATURE_gc_binaries=ON \
+    -DQT_FEATURE_gestures=OFF \
+    -DQT_FEATURE_getauxval=OFF \
+    -DQT_FEATURE_getentropy=OFF \
+    -DQT_FEATURE_gif=OFF \
+    -DQT_FEATURE_glib=OFF \
+    -DQT_FEATURE_glibc=OFF \
+    -DQT_FEATURE_gui=ON \
+    -DQT_FEATURE_harfbuzz=OFF \
+    -DQT_FEATURE_highdpiscaling=ON \
+    -DQT_FEATURE_hijricalendar=ON \
+    -DQT_FEATURE_ico=OFF \
+    -DQT_FEATURE_icu=OFF \
+    -DQT_FEATURE_identityproxymodel=ON \
+    -DQT_FEATURE_im=OFF \
+    -DQT_FEATURE_image_heuristic_mask=ON \
+    -DQT_FEATURE_image_text=ON \
+    -DQT_FEATURE_imageformat_bmp=ON \
+    -DQT_FEATURE_imageformat_jpeg=ON \
+    -DQT_FEATURE_imageformat_png=ON \
+    -DQT_FEATURE_imageformat_ppm=ON \
+    -DQT_FEATURE_imageformat_xbm=ON \
+    -DQT_FEATURE_imageformat_xpm=ON \
+    -DQT_FEATURE_imageformatplugin=ON \
+    -DQT_FEATURE_imageio_text_loading=ON \
+    -DQT_FEATURE_inotify=OFF \
+    -DQT_FEATURE_integrityfb=OFF \
+    -DQT_FEATURE_integrityhid=OFF \
+    -DQT_FEATURE_intelcet=OFF \
+    -DQT_FEATURE_islamiccivilcalendar=ON \
+    -DQT_FEATURE_itemmodel=ON \
+    -DQT_FEATURE_jalalicalendar=ON \
+    -DQT_FEATURE_journald=OFF \
+    -DQT_FEATURE_jpeg=OFF \
+    -DQT_FEATURE_kms=OFF \
+    -DQT_FEATURE_largefile=OFF \
+    -DQT_FEATURE_libinput_axis_api=OFF \
+    -DQT_FEATURE_libinput_hires_wheel_support=OFF \
+    -DQT_FEATURE_libinput=OFF \
+    -DQT_FEATURE_library=OFF \
+    -DQT_FEATURE_libudev=OFF \
+    -DQT_FEATURE_linkat=OFF \
+    -DQT_FEATURE_linuxfb=OFF \
+    -DQT_FEATURE_lttng=OFF \
+    -DQT_FEATURE_mimetype_database=ON \
+    -DQT_FEATURE_mimetype=ON \
+    -DQT_FEATURE_mips_dsp=OFF \
+    -DQT_FEATURE_mips_dspr2=OFF \
+    -DQT_FEATURE_movie=OFF \
+    -DQT_FEATURE_mtdev=OFF \
+    -DQT_FEATURE_multiprocess=OFF \
+    -DQT_FEATURE_neon=OFF \
+    -DQT_FEATURE_network=OFF \
+    -DQT_FEATURE_no_direct_extern_access=OFF \
+    -DQT_FEATURE_opengl=OFF \
+    -DQT_FEATURE_opengles2=ON \
+    -DQT_FEATURE_opengles3=OFF \
+    -DQT_FEATURE_opengles31=OFF \
+    -DQT_FEATURE_opengles32=OFF \
+    -DQT_FEATURE_openssl_hash=OFF \
+    -DQT_FEATURE_openssl_linked=OFF \
+    -DQT_FEATURE_openssl=OFF \
+    -DQT_FEATURE_opensslv11=OFF \
+    -DQT_FEATURE_opensslv30=OFF \
+    -DQT_FEATURE_openvg=OFF \
+    -DQT_FEATURE_pcre2=ON \
+    -DQT_FEATURE_pdf=OFF \
+    -DQT_FEATURE_permissions=OFF \
+    -DQT_FEATURE_picture=ON \
+    -DQT_FEATURE_pkg_config=OFF \
+    -DQT_FEATURE_png=ON \
+    -DQT_FEATURE_poll_exit_on_error=OFF \
+    -DQT_FEATURE_poll_poll=ON \
+    -DQT_FEATURE_poll_pollts=OFF \
+    -DQT_FEATURE_poll_ppoll=OFF \
+    -DQT_FEATURE_poll_select=OFF \
+    -DQT_FEATURE_posix_fallocate=ON \
+    -DQT_FEATURE_posix_sem=OFF \
+    -DQT_FEATURE_posix_shm=OFF \
+    -DQT_FEATURE_precompile_header=ON \
+    -DQT_FEATURE_printsupport=OFF \
+    -DQT_FEATURE_private_tests=OFF \
+    -DQT_FEATURE_process=OFF \
+    -DQT_FEATURE_processenvironment=OFF \
+    -DQT_FEATURE_proxymodel=ON \
+    -DQT_FEATURE_qqnx_imf=OFF \
+    -DQT_FEATURE_qqnx_pps=OFF \
+    -DQT_FEATURE_raster_64bit=ON \
+    -DQT_FEATURE_raster_fp=ON \
+    -DQT_FEATURE_rdrnd=OFF \
+    -DQT_FEATURE_rdseed=OFF \
+    -DQT_FEATURE_reduce_exports=ON \
+    -DQT_FEATURE_reduce_relocations=OFF \
+    -DQT_FEATURE_regularexpression=ON \
+    -DQT_FEATURE_relocatable=ON \
+    -DQT_FEATURE_renameat2=OFF \
+    -DQT_FEATURE_rpath=OFF \
+    -DQT_FEATURE_separate_debug_info=OFF \
+    -DQT_FEATURE_sessionmanager=OFF \
+    -DQT_FEATURE_settings=ON \
+    -DQT_FEATURE_sha3_fast=ON \
+    -DQT_FEATURE_shani=OFF \
+    -DQT_FEATURE_shared=OFF \
+    -DQT_FEATURE_sharedmemory=OFF \
+    -DQT_FEATURE_shortcut=ON \
+    -DQT_FEATURE_signaling_nan=ON \
+    -DQT_FEATURE_simulator_and_device=OFF \
+    -DQT_FEATURE_slog2=OFF \
+    -DQT_FEATURE_sortfilterproxymodel=ON \
+    -DQT_FEATURE_sql=OFF \
+    -DQT_FEATURE_sse2=OFF \
+    -DQT_FEATURE_sse3=OFF \
+    -DQT_FEATURE_sse4_1=OFF \
+    -DQT_FEATURE_sse4_2=OFF \
+    -DQT_FEATURE_ssse3=OFF \
+    -DQT_FEATURE_stack_protector_strong=OFF \
+    -DQT_FEATURE_standarditemmodel=ON \
+    -DQT_FEATURE_static=ON \
+    -DQT_FEATURE_statx=OFF \
+    -DQT_FEATURE_std_atomic64=ON \
+    -DQT_FEATURE_stdlib_libcpp=OFF \
+    -DQT_FEATURE_stringlistmodel=ON \
+    -DQT_FEATURE_syslog=OFF \
+    -DQT_FEATURE_system_doubleconversion=OFF \
+    -DQT_FEATURE_system_freetype=OFF \
+    -DQT_FEATURE_system_harfbuzz=OFF \
+    -DQT_FEATURE_system_jpeg=OFF \
+    -DQT_FEATURE_system_libb2=OFF \
+    -DQT_FEATURE_system_pcre2=OFF \
+    -DQT_FEATURE_system_png=OFF \
+    -DQT_FEATURE_system_textmarkdownreader=OFF \
+    -DQT_FEATURE_system_xcb_xinput=OFF \
+    -DQT_FEATURE_system_zlib=OFF \
+    -DQT_FEATURE_systemsemaphore=OFF \
+    -DQT_FEATURE_systemtrayicon=OFF \
+    -DQT_FEATURE_sysv_sem=OFF \
+    -DQT_FEATURE_sysv_shm=OFF \
+    -DQT_FEATURE_tabletevent=OFF \
+    -DQT_FEATURE_temporaryfile=ON \
+    -DQT_FEATURE_testlib=OFF \
+    -DQT_FEATURE_textdate=ON \
+    -DQT_FEATURE_texthtmlparser=ON \
+    -DQT_FEATURE_textmarkdownreader=ON \
+    -DQT_FEATURE_textmarkdownwriter=ON \
+    -DQT_FEATURE_textodfwriter=ON \
+    -DQT_FEATURE_thread=OFF \
+    -DQT_FEATURE_timezone=OFF \
+    -DQT_FEATURE_translation=OFF \
+    -DQT_FEATURE_transposeproxymodel=OFF \
+    -DQT_FEATURE_tslib=OFF \
+    -DQT_FEATURE_tuiotouch=OFF \
+    -DQT_FEATURE_undocommand=ON \
+    -DQT_FEATURE_undogroup=ON \
+    -DQT_FEATURE_undostack=ON \
+    -DQT_FEATURE_use_bfd_linker=OFF \
+    -DQT_FEATURE_use_gold_linker=OFF \
+    -DQT_FEATURE_use_lld_linker=OFF \
+    -DQT_FEATURE_use_mold_linker=OFF \
+    -DQT_FEATURE_vaes=OFF \
+    -DQT_FEATURE_validator=ON \
+    -DQT_FEATURE_vkgen=OFF \
+    -DQT_FEATURE_vkkhrdisplay=OFF \
+    -DQT_FEATURE_vnc=OFF \
+    -DQT_FEATURE_vsp2=OFF \
+    -DQT_FEATURE_vulkan=OFF \
+    -DQT_FEATURE_wasm_exceptions=OFF \
+    -DQT_FEATURE_wasm_simd128=OFF \
+    -DQT_FEATURE_whatsthis=OFF \
+    -DQT_FEATURE_wheelevent=OFF \
+    -DQT_FEATURE_widgets=OFF \
+    -DQT_FEATURE_x86intrin=ON \
+    -DQT_FEATURE_xcb_egl_plugin=OFF \
+    -DQT_FEATURE_xcb_glx_plugin=OFF \
+    -DQT_FEATURE_xcb_glx=OFF \
+    -DQT_FEATURE_xcb_native_painting=OFF \
+    -DQT_FEATURE_xcb_sm=OFF \
+    -DQT_FEATURE_xcb_xlib=OFF \
+    -DQT_FEATURE_xcb=OFF \
+    -DQT_FEATURE_xkbcommon_x11=OFF \
+    -DQT_FEATURE_xkbcommon=OFF \
+    -DQT_FEATURE_xlib=OFF \
+    -DQT_FEATURE_xml=ON \
+    -DQT_FEATURE_xmlstream=ON \
+    -DQT_FEATURE_xmlstreamreader=ON \
+    -DQT_FEATURE_xmlstreamwriter=ON \
+    -DQT_FEATURE_xrender=OFF \
+    -DQT_FEATURE_zstd=OFF \
+    -DQT_USE_BUNDLED_BundledFreetype=ON \
+    -DQT_USE_BUNDLED_BundledLibpng=ON \
+    -DQT_USE_BUNDLED_BundledPcre2=ON \
+    -DQT_USE_BUNDLED_BundledZLIB=ON
+
+cmake --build ./build/wasm32-wasi --parallel
+cmake --install ./build/wasm32-wasi --prefix "./build/wasm32-wasi-install"
+
+rm -f ./build/wasm32-wasi/qtbase-6.6.1.a && \
+    llvm-ar qcsL ./build/wasm32-wasi/qtbase-6.6.1.a \
+    $(find ./build/wasm32-wasi-install -type f -name "*.obj" -o -name "*.a" | tr '\n' ' ')

reMarkable/qtbase

build.sh
@@ -1,0 +1,347 @@
+#!/usr/bin/env bash
+
+set -e
+set -o xtrace
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_dir"
+
+cmake . \
+    -B ./build/host \
+    -G Ninja \
+    `# Build configuration` \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF \
+    -DQT_BUILD_BENCHMARKS=OFF \
+    -DQT_FEATURE_accessibility=OFF \
+    -DQT_FEATURE_androiddeployqt=OFF \
+    -DQT_FEATURE_animation=OFF \
+    -DQT_FEATURE_backtrace=OFF \
+    -DQT_FEATURE_dbus=OFF \
+    -DQT_FEATURE_debug=OFF \
+    -DQT_FEATURE_developer_build=OFF \
+    -DQT_FEATURE_dom=OFF \
+    -DQT_FEATURE_harfbuzz=OFF \
+    -DQT_FEATURE_ico=OFF \
+    -DQT_FEATURE_icu=OFF \
+    -DQT_FEATURE_network=OFF \
+    -DQT_FEATURE_pdf=OFF \
+    -DQT_FEATURE_timezone=OFF \
+    -DQT_FEATURE_translation=OFF \
+    -DQT_FEATURE_widgets=OFF
+
+
+cmake --build ./build/host --parallel
+cmake --install ./build/host --prefix "./build/host-install"
+
+cmake . \
+    -B ./build/wasm32-wasi \
+    -G Ninja \
+    `# Build configuration` \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_FIND_PACKAGE_TARGETS_GLOBAL=ON \
+    -DQT_HOST_PATH="$script_dir/build/host" \
+    -DQT_HOST_PATH_CMAKE_DIR:PATH="$script_dir/build/host" \
+    -DCMAKE_TOOLCHAIN_FILE="$script_dir/cmake/wasi/wasi-sdk.toolchain.cmake" \
+    -DQT_QMAKE_TARGET_MKSPEC=linux-clang-libc++-32 \
+    -DQT_USE_DEFAULT_CMAKE_OPTIMIZATION_FLAGS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF \
+    -DQT_BUILD_BENCHMARKS=OFF \
+    -DINSTALL_BINDIR:STRING=bin \
+    -DINSTALL_LIBEXECDIR:STRING=bin \
+    -DUNIX:BOOL=ON \
+    -DWASM:BOOL=ON \
+    `# Feature configuration` \
+    -DQT_FEATURE_accessibility_atspi_bridge=OFF \
+    -DQT_FEATURE_accessibility=OFF \
+    -DQT_FEATURE_action=OFF \
+    -DQT_FEATURE_aesni=OFF \
+    -DQT_FEATURE_android_style_assets=OFF \
+    -DQT_FEATURE_androiddeployqt=OFF \
+    -DQT_FEATURE_animation=OFF \
+    -DQT_FEATURE_appstore_compliant=OFF \
+    -DQT_FEATURE_arm_crc32=OFF \
+    -DQT_FEATURE_arm_crypto=OFF \
+    -DQT_FEATURE_avx=OFF \
+    -DQT_FEATURE_avx2=OFF \
+    -DQT_FEATURE_avx512bw=OFF \
+    -DQT_FEATURE_avx512cd=OFF \
+    -DQT_FEATURE_avx512dq=OFF \
+    -DQT_FEATURE_avx512er=OFF \
+    -DQT_FEATURE_avx512f=OFF \
+    -DQT_FEATURE_avx512ifma=OFF \
+    -DQT_FEATURE_avx512pf=OFF \
+    -DQT_FEATURE_avx512vbmi=OFF \
+    -DQT_FEATURE_avx512vbmi2=OFF \
+    -DQT_FEATURE_avx512vl=OFF \
+    -DQT_FEATURE_backtrace=OFF \
+    -DQT_FEATURE_cborstreamreader=ON \
+    -DQT_FEATURE_cborstreamwriter=ON \
+    -DQT_FEATURE_clipboard=OFF \
+    -DQT_FEATURE_clock_gettime=OFF \
+    -DQT_FEATURE_clock_monotonic=OFF \
+    -DQT_FEATURE_close_range=OFF \
+    -DQT_FEATURE_colornames=OFF \
+    -DQT_FEATURE_commandlineparser=ON \
+    -DQT_FEATURE_concatenatetablesproxymodel=ON \
+    -DQT_FEATURE_concurrent=OFF \
+    -DQT_FEATURE_cpp_winrt=OFF \
+    -DQT_FEATURE_cross_compile=ON \
+    -DQT_FEATURE_cssparser=ON \
+    -DQT_FEATURE_ctf=OFF \
+    -DQT_FEATURE_cursor=ON \
+    -DQT_FEATURE_cxx11_future=OFF \
+    -DQT_FEATURE_cxx17_filesystem=OFF \
+    -DQT_FEATURE_cxx20=OFF \
+    -DQT_FEATURE_cxx2a=OFF \
+    -DQT_FEATURE_cxx2b=OFF \
+    -DQT_FEATURE_datestring=ON \
+    -DQT_FEATURE_datetimeparser=ON \
+    -DQT_FEATURE_dbus_linked=OFF \
+    -DQT_FEATURE_dbus=OFF \
+    -DQT_FEATURE_debug_and_release=OFF \
+    -DQT_FEATURE_debug=OFF \
+    -DQT_FEATURE_desktopservices=ON \
+    -DQT_FEATURE_developer_build=OFF \
+    -DQT_FEATURE_direct2d=OFF \
+    -DQT_FEATURE_direct2d1_1=OFF \
+    -DQT_FEATURE_directfb=OFF \
+    -DQT_FEATURE_directwrite=OFF \
+    -DQT_FEATURE_directwrite3=OFF \
+    -DQT_FEATURE_dladdr=OFF \
+    -DQT_FEATURE_dlopen=OFF \
+    -DQT_FEATURE_dom=ON \
+    -DQT_FEATURE_doubleconversion=ON \
+    -DQT_FEATURE_draganddrop=OFF \
+    -DQT_FEATURE_drm_atomic=OFF \
+    -DQT_FEATURE_dynamicgl=OFF \
+    -DQT_FEATURE_easingcurve=ON \
+    -DQT_FEATURE_egl_x11=OFF \
+    -DQT_FEATURE_egl=OFF \
+    -DQT_FEATURE_eglfs_brcm=OFF \
+    -DQT_FEATURE_eglfs_egldevice=OFF \
+    -DQT_FEATURE_eglfs_gbm=OFF \
+    -DQT_FEATURE_eglfs_mali=OFF \
+    -DQT_FEATURE_eglfs_openwfd=OFF \
+    -DQT_FEATURE_eglfs_rcar=OFF \
+    -DQT_FEATURE_eglfs_viv_wl=OFF \
+    -DQT_FEATURE_eglfs_viv=OFF \
+    -DQT_FEATURE_eglfs_vsp2=OFF \
+    -DQT_FEATURE_eglfs_x11=OFF \
+    -DQT_FEATURE_eglfs=OFF \
+    -DQT_FEATURE_etw=OFF \
+    -DQT_FEATURE_evdev=OFF \
+    -DQT_FEATURE_eventfd=OFF \
+    -DQT_FEATURE_f16c=OFF \
+    -DQT_FEATURE_filesystemiterator=OFF \
+    -DQT_FEATURE_filesystemmodel=OFF \
+    -DQT_FEATURE_filesystemwatcher=OFF \
+    -DQT_FEATURE_fontconfig=OFF \
+    -DQT_FEATURE_force_asserts=OFF \
+    -DQT_FEATURE_force_debug_info=OFF \
+    -DQT_FEATURE_forkfd_pidfd=OFF \
+    -DQT_FEATURE_framework=OFF \
+    -DQT_FEATURE_freetype=ON \
+    -DQT_FEATURE_futimens=ON \
+    -DQT_FEATURE_future=OFF \
+    -DQT_FEATURE_gc_binaries=ON \
+    -DQT_FEATURE_gestures=OFF \
+    -DQT_FEATURE_getauxval=OFF \
+    -DQT_FEATURE_getentropy=OFF \
+    -DQT_FEATURE_gif=OFF \
+    -DQT_FEATURE_glib=OFF \
+    -DQT_FEATURE_glibc=OFF \
+    -DQT_FEATURE_gui=ON \
+    -DQT_FEATURE_harfbuzz=OFF \
+    -DQT_FEATURE_highdpiscaling=ON \
+    -DQT_FEATURE_hijricalendar=ON \
+    -DQT_FEATURE_ico=OFF \
+    -DQT_FEATURE_icu=OFF \
+    -DQT_FEATURE_identityproxymodel=ON \
+    -DQT_FEATURE_im=OFF \
+    -DQT_FEATURE_image_heuristic_mask=ON \
+    -DQT_FEATURE_image_text=ON \
+    -DQT_FEATURE_imageformat_bmp=ON \
+    -DQT_FEATURE_imageformat_jpeg=ON \
+    -DQT_FEATURE_imageformat_png=ON \
+    -DQT_FEATURE_imageformat_ppm=ON \
+    -DQT_FEATURE_imageformat_xbm=ON \
+    -DQT_FEATURE_imageformat_xpm=ON \
+    -DQT_FEATURE_imageformatplugin=ON \
+    -DQT_FEATURE_imageio_text_loading=ON \
+    -DQT_FEATURE_inotify=OFF \
+    -DQT_FEATURE_integrityfb=OFF \
+    -DQT_FEATURE_integrityhid=OFF \
+    -DQT_FEATURE_intelcet=OFF \
+    -DQT_FEATURE_islamiccivilcalendar=ON \
+    -DQT_FEATURE_itemmodel=ON \
+    -DQT_FEATURE_jalalicalendar=ON \
+    -DQT_FEATURE_journald=OFF \
+    -DQT_FEATURE_jpeg=OFF \
+    -DQT_FEATURE_kms=OFF \
+    -DQT_FEATURE_largefile=OFF \
+    -DQT_FEATURE_libinput_axis_api=OFF \
+    -DQT_FEATURE_libinput_hires_wheel_support=OFF \
+    -DQT_FEATURE_libinput=OFF \
+    -DQT_FEATURE_library=OFF \
+    -DQT_FEATURE_libudev=OFF \
+    -DQT_FEATURE_linkat=OFF \
+    -DQT_FEATURE_linuxfb=OFF \
+    -DQT_FEATURE_lttng=OFF \
+    -DQT_FEATURE_mimetype_database=ON \
+    -DQT_FEATURE_mimetype=ON \
+    -DQT_FEATURE_mips_dsp=OFF \
+    -DQT_FEATURE_mips_dspr2=OFF \
+    -DQT_FEATURE_movie=OFF \
+    -DQT_FEATURE_mtdev=OFF \
+    -DQT_FEATURE_multiprocess=OFF \
+    -DQT_FEATURE_neon=OFF \
+    -DQT_FEATURE_network=OFF \
+    -DQT_FEATURE_no_direct_extern_access=OFF \
+    -DQT_FEATURE_opengl=OFF \
+    -DQT_FEATURE_opengles2=ON \
+    -DQT_FEATURE_opengles3=OFF \
+    -DQT_FEATURE_opengles31=OFF \
+    -DQT_FEATURE_opengles32=OFF \
+    -DQT_FEATURE_openssl_hash=OFF \
+    -DQT_FEATURE_openssl_linked=OFF \
+    -DQT_FEATURE_openssl=OFF \
+    -DQT_FEATURE_opensslv11=OFF \
+    -DQT_FEATURE_opensslv30=OFF \
+    -DQT_FEATURE_openvg=OFF \
+    -DQT_FEATURE_pcre2=ON \
+    -DQT_FEATURE_pdf=OFF \
+    -DQT_FEATURE_permissions=OFF \
+    -DQT_FEATURE_picture=ON \
+    -DQT_FEATURE_pkg_config=OFF \
+    -DQT_FEATURE_png=ON \
+    -DQT_FEATURE_poll_exit_on_error=OFF \
+    -DQT_FEATURE_poll_poll=ON \
+    -DQT_FEATURE_poll_pollts=OFF \
+    -DQT_FEATURE_poll_ppoll=OFF \
+    -DQT_FEATURE_poll_select=OFF \
+    -DQT_FEATURE_posix_fallocate=ON \
+    -DQT_FEATURE_posix_sem=OFF \
+    -DQT_FEATURE_posix_shm=OFF \
+    -DQT_FEATURE_precompile_header=ON \
+    -DQT_FEATURE_printsupport=OFF \
+    -DQT_FEATURE_private_tests=OFF \
+    -DQT_FEATURE_process=OFF \
+    -DQT_FEATURE_processenvironment=OFF \
+    -DQT_FEATURE_proxymodel=ON \
+    -DQT_FEATURE_qqnx_imf=OFF \
+    -DQT_FEATURE_qqnx_pps=OFF \
+    -DQT_FEATURE_raster_64bit=ON \
+    -DQT_FEATURE_raster_fp=ON \
+    -DQT_FEATURE_rdrnd=OFF \
+    -DQT_FEATURE_rdseed=OFF \
+    -DQT_FEATURE_reduce_exports=ON \
+    -DQT_FEATURE_reduce_relocations=OFF \
+    -DQT_FEATURE_regularexpression=ON \
+    -DQT_FEATURE_relocatable=ON \
+    -DQT_FEATURE_renameat2=OFF \
+    -DQT_FEATURE_rpath=OFF \
+    -DQT_FEATURE_separate_debug_info=OFF \
+    -DQT_FEATURE_sessionmanager=OFF \
+    -DQT_FEATURE_settings=OFF \
+    -DQT_FEATURE_sha3_fast=ON \
+    -DQT_FEATURE_shani=OFF \
+    -DQT_FEATURE_shared=OFF \
+    -DQT_FEATURE_sharedmemory=OFF \
+    -DQT_FEATURE_shortcut=ON \
+    -DQT_FEATURE_signaling_nan=ON \
+    -DQT_FEATURE_simulator_and_device=OFF \
+    -DQT_FEATURE_slog2=OFF \
+    -DQT_FEATURE_sortfilterproxymodel=ON \
+    -DQT_FEATURE_sql=OFF \
+    -DQT_FEATURE_sse2=OFF \
+    -DQT_FEATURE_sse3=OFF \
+    -DQT_FEATURE_sse4_1=OFF \
+    -DQT_FEATURE_sse4_2=OFF \
+    -DQT_FEATURE_ssse3=OFF \
+    -DQT_FEATURE_stack_protector_strong=OFF \
+    -DQT_FEATURE_standarditemmodel=ON \
+    -DQT_FEATURE_static=ON \
+    -DQT_FEATURE_statx=OFF \
+    -DQT_FEATURE_std_atomic64=ON \
+    -DQT_FEATURE_stdlib_libcpp=OFF \
+    -DQT_FEATURE_stringlistmodel=ON \
+    -DQT_FEATURE_syslog=OFF \
+    -DQT_FEATURE_system_doubleconversion=OFF \
+    -DQT_FEATURE_system_freetype=OFF \
+    -DQT_FEATURE_system_harfbuzz=OFF \
+    -DQT_FEATURE_system_jpeg=OFF \
+    -DQT_FEATURE_system_libb2=OFF \
+    -DQT_FEATURE_system_pcre2=OFF \
+    -DQT_FEATURE_system_png=OFF \
+    -DQT_FEATURE_system_textmarkdownreader=OFF \
+    -DQT_FEATURE_system_xcb_xinput=OFF \
+    -DQT_FEATURE_system_zlib=OFF \
+    -DQT_FEATURE_systemsemaphore=OFF \
+    -DQT_FEATURE_systemtrayicon=OFF \
+    -DQT_FEATURE_sysv_sem=OFF \
+    -DQT_FEATURE_sysv_shm=OFF \
+    -DQT_FEATURE_tabletevent=OFF \
+    -DQT_FEATURE_temporaryfile=ON \
+    -DQT_FEATURE_testlib=ON \
+    -DQT_FEATURE_textdate=ON \
+    -DQT_FEATURE_texthtmlparser=ON \
+    -DQT_FEATURE_textmarkdownreader=ON \
+    -DQT_FEATURE_textmarkdownwriter=ON \
+    -DQT_FEATURE_textodfwriter=ON \
+    -DQT_FEATURE_thread=OFF \
+    -DQT_FEATURE_timezone=OFF \
+    -DQT_FEATURE_translation=OFF \
+    -DQT_FEATURE_transposeproxymodel=OFF \
+    -DQT_FEATURE_tslib=OFF \
+    -DQT_FEATURE_tuiotouch=OFF \
+    -DQT_FEATURE_undocommand=ON \
+    -DQT_FEATURE_undogroup=ON \
+    -DQT_FEATURE_undostack=ON \
+    -DQT_FEATURE_use_bfd_linker=OFF \
+    -DQT_FEATURE_use_gold_linker=OFF \
+    -DQT_FEATURE_use_lld_linker=OFF \
+    -DQT_FEATURE_use_mold_linker=OFF \
+    -DQT_FEATURE_vaes=OFF \
+    -DQT_FEATURE_validator=ON \
+    -DQT_FEATURE_vkgen=OFF \
+    -DQT_FEATURE_vkkhrdisplay=OFF \
+    -DQT_FEATURE_vnc=OFF \
+    -DQT_FEATURE_vsp2=OFF \
+    -DQT_FEATURE_vulkan=OFF \
+    -DQT_FEATURE_wasm_exceptions=OFF \
+    -DQT_FEATURE_wasm_simd128=OFF \
+    -DQT_FEATURE_whatsthis=OFF \
+    -DQT_FEATURE_wheelevent=OFF \
+    -DQT_FEATURE_widgets=OFF \
+    -DQT_FEATURE_x86intrin=ON \
+    -DQT_FEATURE_xcb_egl_plugin=OFF \
+    -DQT_FEATURE_xcb_glx_plugin=OFF \
+    -DQT_FEATURE_xcb_glx=OFF \
+    -DQT_FEATURE_xcb_native_painting=OFF \
+    -DQT_FEATURE_xcb_sm=OFF \
+    -DQT_FEATURE_xcb_xlib=OFF \
+    -DQT_FEATURE_xcb=OFF \
+    -DQT_FEATURE_xkbcommon_x11=OFF \
+    -DQT_FEATURE_xkbcommon=OFF \
+    -DQT_FEATURE_xlib=OFF \
+    -DQT_FEATURE_xml=ON \
+    -DQT_FEATURE_xmlstream=ON \
+    -DQT_FEATURE_xmlstreamreader=ON \
+    -DQT_FEATURE_xmlstreamwriter=ON \
+    -DQT_FEATURE_xrender=OFF \
+    -DQT_FEATURE_zstd=OFF \
+    -DQT_USE_BUNDLED_BundledFreetype=ON \
+    -DQT_USE_BUNDLED_BundledLibpng=ON \
+    -DQT_USE_BUNDLED_BundledPcre2=ON \
+    -DQT_USE_BUNDLED_BundledZLIB=ON
+
+cmake --build ./build/wasm32-wasi --parallel
+cmake --install ./build/wasm32-wasi --prefix "./build/wasm32-wasi-install"
+
+# Archive the install directory
+pushd ./build/wasm32-wasi-install && tar -czvf ../qtbase-wasm32-wasi-install.tar.gz . && popd